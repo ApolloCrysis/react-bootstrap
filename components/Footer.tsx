9d7da8f85debb0097393a9ef3ad2928e512e56c7
ApolloCrysis <djvdjondre@gmail.com>
2022-01-10 15:58:09 +0200

    Added footer component Added nav component Added home Added users Added Login Added bootstrap react

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            2Way Systems
+        </div>
+    );
+}
+
+export default Footer
---
+++ pages/users/userDetail.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+function UserDetail() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>User Detail Page</h1>
+        </div>
+    )
+}
+
+export default UserDetail;
